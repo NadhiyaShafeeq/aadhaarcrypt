f6fc1af57c30c793076fcca8cdf849ab33f3b6c1
shibli <mohdshibli27@gmail.com>
2019-03-20 18:22:33 +0530

    other requests blocked

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, Response, request
 import Engine
 import json
 from flask_cors import CORS
+import base64
+import argparse
 
 app = Flask(__name__)
 CORS(app)
@@ -10,10 +12,9 @@ CORS(app)
 def generate_token():
     if request.method == "GET":
         return Response('{"error" : "post data not sent"}', mimetype="application/json")
-    else:
+    elif request.method == "POST":
         data = request.data
         data = data.decode('utf-8')
-        print(data)
         details = {
             "name" : data[0],
             "email" : data[1]
@@ -23,5 +24,48 @@ def generate_token():
             "key" : user.generate_key
         }
         return Response(json.dumps(resp), mimetype="application/json")
+    else:
+        return Response("{\"error\" : \"request not allowed\"}", mimetype="application/json")
+
+
+@app.route('/encrypt-data', methods=["POST", "GET"])
+def encrypt_data():
+    if request.method == "GET":
+        return Response('{"error" : "post data not sent"}', mimetype="application/json")
+    elif request.method == "POST":
+        data = request.data
+        data = data.decode('utf-8')
+        data = json.loads(data)
+        key = data["key"]
+        print("This is key:"+key)
+        details = {
+            "aadhaarno" : data["aadhaarno"],
+            "name" : data["name"],
+            "dob" : data["dob"],
+            "address" : data["address"]
+        }
+        card = Engine.Card(key, details)
+
+        resp = {"encrypted_text" : card.encrypt_data}
+        return Response(json.dumps(resp), mimetype="application/json")
+    else:
+        return Response("{\"error\" : \"request not allowed\"}", mimetype="application/json")
+
+@app.route('/decrypt-data', methods=["POST", "GET"])
+def decrypt_data():
+    if request.method == "GET":
+        return Response('{"error" : "post data not sent"}', mimetype="application/json")
+    elif request.method == "POST":
+        data = request.data
+        data = data.decode('utf-8')
+        data = json.loads(data)
+        key = data["key"]
+
+        card = Engine.Card(key)
+        print(key)
+        resp = {"decrypted_text" : card.decrypt_data(key, data["data"])}
+        return Response(json.dumps(resp), mimetype="application/json")
+    else:
+        return Response("{\"error\" : \"request not allowed\"}", mimetype="application/json")
 
 app.run(debug=True)
