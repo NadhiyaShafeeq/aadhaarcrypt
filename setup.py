7558b36864d7eb1f76b9b6539499cb3066d02fce
Mohd Shibli Qureshi <mohdshibli27@gmail.com>
2019-06-30 11:57:58 +0530

    pycryptodome added as dependency for windows users

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "AadhaarCrypt",
     version = "0.1",
     license = "GPL-3.0",
-    install_requires = ["flask_cors", "flask", "pycrypto"],
+    install_requires = ["flask_cors", "flask", "pycrypto", "pycryptodome"],
     long_description = open("README.md").read()
 )
